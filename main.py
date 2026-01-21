e3c0007f079c3e95a2308f69475da4730685093d
mark <139384872+markart25@users.noreply.github.com>
2026-01-21 20:06:20 +0000

    new code type shit

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 secret_role = "Gamer"
 
-@bot.command()
-async def info(ctx):
-    await ctx.send(f"{ctx.author.mention} remember !info to show this panel \n !dice rolls a 6 sided dice \n !dicesides x rolls a x sided dice \n !dicemm x y rolls a dice between x and y \n !coinflip flips a coin")
-
 @bot.event
 async def on_ready():
     print(f"We are ready to go in, {bot.user.name}")
@@ -41,6 +37,8 @@ async def dicesides(ctx, *, msg):
     sides = int(str(msg).strip())
     if sides == 0:
         await ctx.send(f"{ctx.author.mention} i cant roll a 0 sided dice bro")
+    elif sides < 0:
+        await ctx.send(f"{ctx.author.mention} i cant roll a negative sided dice bro, lock tf in")
     elif sides == 1:
         await ctx.send(f"{ctx.author.mention} i cant roll a 1 sided dice bro")
     elif sides > 1000000:
@@ -51,7 +49,9 @@ async def dicesides(ctx, *, msg):
 
 @bot.command()
 async def dicemm(ctx, *, msg):
+    print(msg)
     list = str(msg).strip().split(" ")
+    print(list)
     minside = int(list[0])
     maxside = int(list[1])
     if minside >= maxside:
@@ -74,7 +74,44 @@ async def coinflip(ctx):
     if result == 2:
         await ctx.send(f"{ctx.author.mention} mate you flipped a **tail** nice one")
 
+@bot.command()
+async def goon(ctx):
+    await ctx.send(f"{ctx.author.mention} bro what r u on about lock tf in gang")
 
+@bot.command()
+async def looksmax(ctx):
+    result = random.randint(1,100)
+    if result >= 95:
+        await ctx.send(f"{ctx.author.mention} true adam")
+    elif result >= 90:
+        await ctx.send(f"{ctx.author.mention} chad")
+    elif result >= 85:
+        await ctx.send(f"{ctx.author.mention} chad lite")
+    elif result >= 75:
+        await ctx.send(f"{ctx.author.mention} htn")
+    elif result >= 65:
+        await ctx.send(f"{ctx.author.mention} mtn")
+    elif result >= 55:
+        await ctx.send(f"{ctx.author.mention} ltn")
+    elif result >= 40:
+        await ctx.send(f"{ctx.author.mention} sub 5")
+    elif result >= 20:
+        await ctx.send(f"{ctx.author.mention} sub 3")
+    else:   
+        await ctx.send(f"{ctx.author.mention} sub human")
+   
+@bot.command()
+async def job(ctx):
+    await ctx.send(f"{ctx.author.mention} you can find jobs here: https://www.seek.com.au/ or here: https://www.linkedin.com/jobs/ ")
+   
+@bot.command()
+async def elkin(ctx):
+    await ctx.send(f"{ctx.guild.get_member(1053413965637623910).mention} elkin is a legend and a top bloke, so much aura, true adam")
 
+@bot.command()
+async def mark(ctx):
+    await ctx.send(f"{ctx.guild.get_member(810532743653228654).mention} mark is cool, creator of this amazing bot, mtn")
+ 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
 
